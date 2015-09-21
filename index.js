da34ff478a86759e85aef5c92e1b8982d8c74203
djfm <fm.de.jouvencel@gmail.com>
2015-09-21 09:42:21 +0200

    Added onupdate option to allow for watching

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 'use strict';
 
 var through        = require('through2');
-var stylus         = require('accord').load('stylus');
+var stylus         = require('stylus');
 var gutil          = require('gulp-util');
 var rext           = require('replace-ext');
 var path           = require('path');
 var assign         = require('lodash.assign');
 var applySourceMap = require('vinyl-sourcemaps-apply');
+var watch          = require('node-watch');
 
 var PLUGIN_NAME = 'gulp-stylus';
 
 module.exports = function (options) {
+  var onupdate;
+  if (options) {
+    onupdate = options.onupdate;
+    delete options.onupdate;
+  }
+
   var opts = assign({}, options);
 
   return through.obj(function (file, enc, cb) {
@@ -29,25 +36,28 @@ module.exports = function (options) {
     }
     opts.filename = file.path;
 
-    stylus.render(file.contents.toString('utf8'), opts)
-      .then(function(res) {
-        if (res.result !== undefined) {
-          file.path = rext(file.path, '.css');
-          file.contents = new Buffer(res.result);
-          if (res.sourcemap) {
-            makePathsRelative(file, res.sourcemap);
-            res.sourcemap.file = file.relative;
-            applySourceMap(file, res.sourcemap);
-          }
-          return cb(null, file);
-        }
-      })
-      .catch(function(err) {
-        delete err.input;
-        return cb(new gutil.PluginError(PLUGIN_NAME, err));
-      });
-  });
+    var style = stylus(file.contents.toString('utf8'), opts);
 
+    style.render(function (err, css) {
+      if (err) {
+        delete err.input;
+        cb(new gutil.PluginError(PLUGIN_NAME, err));
+      } else {
+        file.path = rext(file.path, '.css');
+        file.contents = new Buffer(css);
+        if (style.sourcemap) {
+          makePathsRelative(file, style.sourcemap);
+          style.sourcemap.file = file.relative;
+          applySourceMap(file, style.sourcemap);
+        }
+        cb(null, file);
+        if (onupdate) {
+          var deps = style.deps();
+          watch(deps, onupdate);
+        }
+      }
+    });
+  });
 };
 
 function makePathsRelative(file, sourcemap) {
